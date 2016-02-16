8951f7c80fe23e6de3763dcdccc994b327ab9dc3
limelime <limelime@users.noreply.github.com>
2016-02-16 15:01:14 -0500

    Fix error handling.

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 set -e
-# Description: Copy from SOURCE/ to DESTINATION/ and delete extraneous files from the receiving side. 
+# Description: Copy from SOURCE/ to DESTINATION/ and delete extraneous files from the receiving side.
 
-SOURCE_DIR=$1
-DEST_DIR=$2
-EXCLUDE_LIST=$2
+ACTION=$1
+SOURCE_DIR=$2
+DEST_DIR=$3
+EXCLUDE_LIST=$4
 
 EXCLUDE_FROM_OPTION=""
+
 # Error handling
-if [ ! -d "${SOURCE_DIR}" ]; then
-  echo "$0: Error: Source directory: ${SOURCE_DIR}: no such directory. Aborted!"
-  exit 1;
-fi
-
-if [ ! -d "${DEST_DIR}" ]; then
-  echo "$0: Error: Destination directory: ${DEST_DIR}: no such directory. Aborted!"
-  exit 1;
-fi
-
-if [ ! -z "${EXCLUDE_LIST}" ]; then
-	if [ ! -f "${EXCLUDE_LIST}" ]; then
-	  echo "$0: Error: Exclude list: ${EXCLUDE_LIST}: no such file. Aborted!"
+  CMD_EXAMPLES=$(printf " %s\n %s\n %s\n" \
+                        " e.g. $0 <ACTION> <SOURCE_DIR> <DESTINATION_DIR> [EXCLUDE_LIST]"\
+                        " e.g. $0 try /some/source/ /some/destination/"\
+                        " e.g. $0 commit /some/source/ /some/destination/ exclude.txt"\
+                )
+	if [ -z "${ACTION}" ]; then
+	  echo "$0: Error: Action can't be empty. Aborted!"
+	  echo "${CMD_EXAMPLES}"
 	  exit 1;
 	fi
-fi
+	
+	if [ ! -d "${SOURCE_DIR}" ]; then
+	  echo "$0: Error: Source directory: ${SOURCE_DIR}: no such directory. Aborted!"
+	  echo "${CMD_EXAMPLES}"
+	  exit 1;
+	fi
+	
+	if [ ! -d "${DEST_DIR}" ]; then
+	  echo "$0: Error: Destination directory: ${DEST_DIR}: no such directory. Aborted!"
+	  echo "${CMD_EXAMPLES}"
+	  exit 1;
+	fi
+	
+	if [ ! -z "${EXCLUDE_LIST}" ]; then
+		if [ ! -f "${EXCLUDE_LIST}" ]; then
+		  echo "$0: Error: Exclude list file: ${EXCLUDE_LIST}: no such file. Aborted!"
+		  echo "${CMD_EXAMPLES}"
+		  exit 1;
+		else
+		  # Construct exclude option.
+		  EXCLUDE_FROM_OPTION="--exclude-from=${EXCLUDE_LIST}"	
+		  EXCLUDE_LIST=$(readlink -ev "${EXCLUDE_LIST}")
+	  fi
+	fi
+	
+	SOURCE_DIR=$(readlink -ev "${SOURCE_DIR}")
+	DEST_DIR=$(readlink -ev "${DEST_DIR}")
 
-SOURCE_DIR=$(readlink -ev "${SOURCE_DIR}")
-DEST_DIR=$(readlink -ev "${DEST_DIR}")
-EXCLUDE_LIST=$(readlink -ev "${EXCLUDE_LIST}")
-
-
-# Construct exclude option.
-EXCLUDE_FROM_OPTION="--exclude-from=${EXCLUDE_LIST}"
 
 # Create deploy log directory.
 DEPLOY_LOG_DIR="./deploy_logs"
@@ -40,17 +56,21 @@ DEPLOY_LOG_DIR=$(readlink -ev "${DEPLOY_LOG_DIR}")
 
 # Deploy
 DATE_STRING=$(date +"%Y-%m-%d_%0k.%M.%S")
-ACTION=$1
 case "${ACTION}" in
   
   commit)
     # Really commit deployment. 
     rsync -a --checksum --delete-after --progress --itemize-changes --stats --out-format='%t %p %i %n %M %l' "${EXCLUDE_FROM_OPTION}" "${SOURCE_DIR}/" "${DEST_DIR}/" > "${DEPLOY_LOG_DIR}/deploy_${DATE_STRING}.log"
+    echo "Deployment log created at ${DEPLOY_LOG_DIR}/deploy_${DATE_STRING}.log."
+    ;;
+    
+  try)
+    rsync -a --checksum --delete-after --progress --itemize-changes --stats --out-format='%t %p %i %n %M %l' --dry-run "${EXCLUDE_FROM_OPTION}" "${SOURCE_DIR}/" "${DEST_DIR}/"
     ;;
     
   *)
-    rsync -a --checksum --delete-after --progress --itemize-changes --stats --out-format='%t %p %i %n %M %l' --dry-run "${EXCLUDE_FROM_OPTION}" "${SOURCE_DIR}/" "${DEST_DIR}/"
-    ;;
+    echo "Error: ${ACTION}: Unknown action. Aborted!"
+    exit 1
+    ;;    
 esac
 
-echo "Deployment log created at ${DEPLOY_LOG_DIR}/deploy_${DATE_STRING}.log."
