50a2e4e8e428b770f9614f86260ab1a3f300d7a4
limelime <limelime@users.noreply.github.com>
2018-04-21 19:05:00 +0000

    improve update firstboot script.

--- update-fb-re-use-x.sh
+++ update-fb-re-use-x.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
-# Description: Update Re-use-x in firstboot.
+# Description: Update re-use-x in firstboot.
 
-fb_re_use_x_run_dir=/media/master/github/firstboot/apps/re-use-x/run
+fb_run_dir=$(readlink -ev /media/master/github/firstboot/apps/re-use-x/run)
+re_use_x_script_dir=$(readlink -ev ./re-use-x)
 
-rsync -r re-use-x/ ${fb_re_use_x_run_dir}
+# Update re-use-x to firstboot.
+  rsync -a --delete ${re_use_x_script_dir}/ ${fb_run_dir}
+
+# Commit re-use-x at firstboot.
+  (
+    cd "${fb_run_dir}"
+    git ls-files --deleted -z | xargs -r -0 git rm && git commit -m 're-use-x: commit deleted files.' || true
+    git ls-files --modified -z | xargs -r -0 git commit -m 're-use-x: commit changed files.'
+    git ls-files --others -z | xargs -r -0 git add && git commit -m 're-use-x: commit new files.' || true
+  )
